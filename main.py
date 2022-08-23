14631b4abe185e1ab6515b689c68813e7cd01cc1
7 <88673481+7gh@users.noreply.github.com>
2022-08-23 08:12:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-going to upload code tommorrow
+import discord, random, json
+from discord.ext import commands
+
+token = open("token.txt", "r").read()
+bot = commands.Bot(command_prefix=".")
+
+bot.run(token)
